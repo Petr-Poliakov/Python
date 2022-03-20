8a16ae426a71db8c5252fb71bbac2023fd9805e2
Petr-Poliakov <94320840+Petr-Poliakov@users.noreply.github.com>
2022-03-20 08:03:12 +0300

    library and branch

---
+++ log.py
@@ -0,0 +1,9 @@
+from bdb import effective
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+def log_command(update: Update, context: CallbackContext):
+    file = open('log.csv', 'a')
+    file.write = (f'{update.effective_user.first_name},{update.effective_user.id},{update.message.text}\n')
+    file.close()
+
